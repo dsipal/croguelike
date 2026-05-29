1245c6b998dea4b462978831cc8ddf88430b496f
Devin Sipal <devin@devinsipal.com>
2026-05-29 12:49:09 -0400

    initial bsptree map

--- src/Map.cpp
+++ src/Map.cpp
@@ -1,113 +1,115 @@
-#include "libtcod.hpp"
 #include "Map.hpp"
+#include "libtcod.hpp"
+#include <libtcod/bsp.hpp>
 #include <libtcod/color.hpp>
 
-Room generateRoom(int screen_width, int screen_height, const std::vector<Room>& rooms) {
-    TCODRandom *rng = TCODRandom::getInstance();
- 
-    //function will run until it generates a room with no overlaps to previous rooms
-    while (true) {
-        Room room = {rng->getInt(1,
-            screen_width - 20),
-            rng->getInt(1, screen_height - 20),
-            rng->getInt(3, 20),
-            rng->getInt(3, 20)};
+Room generateRoom(int width, int height) {
+  TCODRandom *rng = TCODRandom::getInstance();
 
+  // function will run until it generates a room with no overlaps to previous
+  // rooms
+  Room room = {rng->getInt(1, width - 20), rng->getInt(1, height - 20),
+               rng->getInt(3, 20), rng->getInt(3, 20)};
 
-        bool overlaps = false;
-
-        for (const auto& other_room : rooms) {
-            if (room.x < other_room.x + other_room.width &&
-                room.x + room.width > other_room.x &&
-                room.y < other_room.y + other_room.height &&
-                room.y + room.height > other_room.y) {
-                overlaps = true;
-                break;
-            }
-        }
-        
-        if (!overlaps) return room;
-    }
+  return room;
 }
 
 void Map::drunkardsWalk() {
-    TCODRandom *rng = TCODRandom::getInstance();
-    for (int i = 0; i < (int)rooms.size() - 1; i++) {
-        int x = rooms[i].center_x();
-        int y = rooms[i].center_y();
-        int tx = rooms[i + 1].center_x();
-        int ty = rooms[i + 1].center_y();
-        while (x != tx || y != ty) {
-            setFloor(x, y);
-            setFloor(x+1,y+1);
-            if (x == tx) {
-                y += (ty > y) ? 1 : -1;
-            } else if (y == ty) {
-                x += (tx > x) ? 1 : -1;
-            } else if (rng->getInt(0, 1)) {
-                x += (tx > x) ? 1 : -1;
-            } else {
-                y += (ty > y) ? 1 : -1;
-            }
-        }
-        setFloor(x, y);
+  TCODRandom *rng = TCODRandom::getInstance();
+  for (int i = 0; i < (int)rooms.size() - 1; i++) {
+    int x = rooms[i].center_x();
+    int y = rooms[i].center_y();
+    int tx = rooms[i + 1].center_x();
+    int ty = rooms[i + 1].center_y();
+    while (x != tx || y != ty) {
+      setFloor(x, y);
+      setFloor(x + 1, y + 1);
+      if (x == tx) {
+        y += (ty > y) ? 1 : -1;
+      } else if (y == ty) {
+        x += (tx > x) ? 1 : -1;
+      } else if (rng->getInt(0, 1)) {
+        x += (tx > x) ? 1 : -1;
+      } else {
+        y += (ty > y) ? 1 : -1;
+      }
     }
+    setFloor(x, y);
+  }
 }
 
-Map::Map(int width, int height, int room_count) : width(width), height(height), room_count(room_count) {
-    tiles = new Tile[width * height];
-    for (int i = 0; i < room_count; i++) {
-        rooms.push_back(generateRoom(width, height, rooms));
-    }
-    drunkardsWalk();
+class BspListener : public ITCODBspCallback {
+private:
+  Map &map;
+  int room_count;
+  int lastx, lasty;
+  TCODRandom *rng = TCODRandom::getInstance();
+
+public:
+  BspListener(Map &map) : map(map), room_count(0) {}
+
+  bool visitNode(TCODBsp *node, void *) override {
+    if (node->isLeaf()) {
+      Room room = generateRoom(node->w - rng->getInt(3, 10),
+                               node->h - rng->getInt(3, 10));
+      map.rooms.push_back(room);
+      return true;
+    } else
+      return false;
+  }
+};
+
+Map::Map(int width, int height, int room_count)
+    : width(width), height(height), room_count(room_count) {
+  tiles = new Tile[width * height];
+  TCODBsp bsp(0, 0, width, height);
+  bsp.splitRecursive(NULL, 16, 16, 16, 2.0f, 1.5f);
+  BspListener listener(*this);
+
+  // iterate through the bsp and run the listener function
+  bsp.traverseInvertedLevelOrder(&listener, NULL);
+
+  //drunkardsWalk();
 }
-Map::~Map() {
-    delete [] tiles;
-}
-std::pair<int,int> Map::getPlayerStart() const {
-    return {rooms[0].center_x(), rooms[0].center_y()};
+Map::~Map() { delete[] tiles; }
+std::pair<int, int> Map::getPlayerStart() const {
+  return {rooms[0].center_x(), rooms[0].center_y()};
 }
 
-bool Map::isWall(int x, int y) const {
-    return !tiles[x+y*width].walkable;
-}
+bool Map::isWall(int x, int y) const { return !tiles[x + y * width].walkable; }
 
-void Map::setWall(int x, int y) {
-    tiles[x+y*width].walkable=false;
-}
+void Map::setWall(int x, int y) { tiles[x + y * width].walkable = false; }
 
-void Map::setFloor(int x, int y) {
-    tiles[x+y*width].walkable=true;
-}
+void Map::setFloor(int x, int y) { tiles[x + y * width].walkable = true; }
 
 void Map::drawRoom(Room room) {
-    for (int x = room.x; x < room.x + room.width; x++) {
-        for (int y = room.y; y < room.y + room.height; y++) {
-            if (x >= 0 && x < width && y >= 0 && y < height) {
-                Tile& tile = tiles[x + y * width];
-                tile.walkable = true;
-                tile.fg = {255, 255, 255};
-            }
-        }
+  for (int x = room.x; x < room.x + room.width; x++) {
+    for (int y = room.y; y < room.y + room.height; y++) {
+      if (x >= 0 && x < width && y >= 0 && y < height) {
+        Tile &tile = tiles[x + y * width];
+        tile.walkable = true;
+        tile.fg = {255, 255, 255};
+      }
     }
+  }
 }
 
-void Map::render(tcod::Console& console) {
-    for (const Room& room : rooms) {
-        drawRoom(room);
-    }
-    static const TCOD_ColorRGB darkWall{0,0,100};
-    static const TCOD_ColorRGB darkGround{50,50,150};
-    for (int x=0; x < width; x++) {
-        for (int y=0; y < height; y++) {
-            if (console.in_bounds({x, y})) {
-                const Tile& tile = tiles[x + y * width];
-                console.at({x, y}).bg = isWall(x,y) ? darkWall : darkGround;
-                if (tile.ch) {
-                    console.at({x, y}).ch = tile.ch;
-                    console.at({x, y}).fg = tile.fg;
-                }
-            }
+void Map::render(tcod::Console &console) {
+  for (const Room &room : rooms) {
+    drawRoom(room);
+  }
+  static const TCOD_ColorRGB darkWall{0, 0, 100};
+  static const TCOD_ColorRGB darkGround{50, 50, 150};
+  for (int x = 0; x < width; x++) {
+    for (int y = 0; y < height; y++) {
+      if (console.in_bounds({x, y})) {
+        const Tile &tile = tiles[x + y * width];
+        console.at({x, y}).bg = isWall(x, y) ? darkWall : darkGround;
+        if (tile.ch) {
+          console.at({x, y}).ch = tile.ch;
+          console.at({x, y}).fg = tile.fg;
         }
+      }
     }
+  }
 }
--- src/Map.hpp
+++ src/Map.hpp
@@ -1,38 +1,41 @@
 #pragma once
-#include <vector>
-#include <utility>
+#include <libtcod/bsp.hpp>
 #include <libtcod/color.hpp>
 #include <libtcod/console.hpp>
+#include <utility>
+#include <vector>
 
 struct Tile {
-    bool walkable;
-    int ch;
-    TCOD_ColorRGB fg;
-    Tile() : walkable(false), ch(0), fg({255, 255, 255}) {}
+  bool walkable;
+  int ch;
+  TCOD_ColorRGB fg;
+  Tile() : walkable(false), ch(0), fg({255, 255, 255}) {}
 };
 
 struct Room {
-    int x, y, width, height;
-    Room(int x, int y, int width, int height) : x(x), y(y), width(width), height(height) {}
-    int center_x() const { return x + width / 2; }
-    int center_y() const { return y + height / 2; }
+  int x, y, width, height;
+  Room(int x, int y, int width, int height)
+      : x(x), y(y), width(width), height(height) {}
+  int center_x() const { return x + width / 2; }
+  int center_y() const { return y + height / 2; }
 };
 
 class Map {
-    public:
-        int width, height, room_count;
-        Map(int width, int height, int room_count);
-        ~Map();
-        bool isWall(int x, int y) const;
-        void render(tcod::Console& console);
-        std::pair<int,int> getPlayerStart() const;
+public:
+  int width, height, room_count;
+  TCODBsp bsp;
+  Map(int width, int height, int room_count);
+  ~Map();
+  bool isWall(int x, int y) const;
+  void render(tcod::Console &console);
+  std::pair<int, int> getPlayerStart() const;
 
-
-    protected:
-        Tile *tiles;
-        std::vector<Room> rooms;
-        void setWall(int x, int y);
-        void setFloor(int x, int y);
-        void drawRoom(Room room);
-        void drunkardsWalk();
+protected:
+  Tile *tiles;
+  friend class BspListener;
+  std::vector<Room> rooms;
+  void setWall(int x, int y);
+  void setFloor(int x, int y);
+  void drawRoom(Room room);
+  void drunkardsWalk();
 };
